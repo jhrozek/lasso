84bc5b48d3094cd6d69c562eb70e62dd7c81d796
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2019-09-06 14:42:44 +0200

    Improve compatibility with Solaris (#32425)

--- lasso/xml/tools.c
+++ lasso/xml/tools.c
@@ -28,7 +28,9 @@
  */
 #define _DEFAULT_SOURCE
 /* permit importation of strptime for glibc2 */
+#if !defined(__sun)
 #define _XOPEN_SOURCE
+#endif
 /* permit importation of timegm for glibc2, wait for people to complain it does not work on their
  * system. */
 #define _BSD_SOURCE
